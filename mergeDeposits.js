1565437fb32fce37b2f4d89307d01d8e74948cf1
ankitboghra <ankitboghra@gmail.com>
2022-01-13 15:05:30 +0530

    rft: create deposit withdraw modules

---
+++ mergeDeposits.js
@@ -0,0 +1,34 @@
+function mergeDeposits(openapiDeposits, watchgodDeposits) {
+    const mergedDeposits = {};
+
+    watchgodDeposits.forEach((tx) => {
+        const txHash = tx.txHash;
+        mergedDeposits[txHash] = tx;
+        mergedDeposits[txHash].latestStatus = tx.txStatus;
+        mergedDeposits[txHash].watchgodTxStatus = tx.txStatus;
+        mergedDeposits[txHash].txSource = "watchgod_deposits";
+    });
+
+    openapiDeposits.forEach((tx) => {
+        const txHash = tx.txHash;
+        const watchgodTxStatus = mergedDeposits[txHash]
+            ? mergedDeposits[txHash].txStatus
+            : false;
+
+        mergedDeposits[txHash] = tx;
+        mergedDeposits[txHash].latestStatus = tx.txStatus;
+        mergedDeposits[txHash].openapiTxStatus = tx.txStatus;
+
+        if (watchgodTxStatus) {
+            mergedDeposits[txHash].watchgodTxStatus = watchgodTxStatus;
+        }
+
+        mergedDeposits[txHash].txSource = "openapi_deposits";
+    });
+
+    return Object.values(mergedDeposits);
+}
+
+module.exports = {
+    mergeDeposits
+}
