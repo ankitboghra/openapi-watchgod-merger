5f0935fb1f6ea85dafe1c5cb1489633a38e3ce08
ankitboghra <ankitboghra@gmail.com>
2022-01-13 15:48:11 +0530

    new: export variables from library

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 const merger = require("./mergeOpenApiWatchgod");
+const { TX_SOURCE, TX_TYPE } = require("./constants");
 
-const {
-    openApiDepositResponse,
-    openApiWithdrawResponse,
-    openApiBurnResponse,
-    watchGodResponse,
-} = require("../responseData");
+// const {
+//     openApiDepositResponse,
+//     openApiWithdrawResponse,
+//     openApiBurnResponse,
+//     watchGodResponse,
+// } = require("../responseData");
 
-const mergedData = merger(
-    openApiDepositResponse,
-    openApiWithdrawResponse,
-    openApiBurnResponse,
-    watchGodResponse
-);
+// const mergedData = merger(
+//     openApiDepositResponse,
+//     openApiWithdrawResponse,
+//     openApiBurnResponse,
+//     watchGodResponse
+// );
 
-console.log(mergedData);
+module.exports = {
+    mergeOpenapiWatchgod: merger,
+    TX_SOURCE,
+    TX_TYPE,
+}
