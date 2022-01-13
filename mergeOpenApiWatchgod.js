5c3d6ddaed93291481824836741c809115c48b40
ankitboghra <ankitboghra@gmail.com>
2022-01-13 15:05:30 +0530

    rft: create deposit withdraw modules

--- mergeOpenApiWatchgod.js
+++ mergeOpenApiWatchgod.js
@@ -51,6 +51,10 @@ const {
     convertWatchgodToInterface,
     convertInterfaceToWatchgod,
 } = require("./utils");
+
+const { mergeDeposits } = require('./mergeDeposits')
+const { mergeWithdraws } = require('./mergeWithdraws')
+
 /**
  *
  * @param {*} openapiDepositResponse
@@ -92,143 +96,6 @@ function merger(
         ...mergeDeposits(openapiDeposits, watchgodDeposits),
         ...mergeWithdraws(openapiExits, openapiBurns, watchgodExits, watchgodBurns),
     ];
-}
-
-function mergeDeposits(openapiDeposits, watchgodDeposits) {
-    const mergedDeposits = {};
-
-    watchgodDeposits.forEach((tx) => {
-        const txHash = tx.txHash;
-        mergedDeposits[txHash] = tx;
-        mergedDeposits[txHash].latestStatus = tx.txStatus;
-        mergedDeposits[txHash].watchgodTxStatus = tx.txStatus;
-        mergedDeposits[txHash].txSource = "watchgod_deposits";
-    });
-
-    openapiDeposits.forEach((tx) => {
-        const txHash = tx.txHash;
-        const watchgodTxStatus = mergedDeposits[txHash]
-            ? mergedDeposits[txHash].txStatus
-            : false;
-
-        mergedDeposits[txHash] = tx;
-        mergedDeposits[txHash].latestStatus = tx.txStatus;
-        mergedDeposits[txHash].openapiTxStatus = tx.txStatus;
-
-        if (watchgodTxStatus) {
-            mergedDeposits[txHash].watchgodTxStatus = watchgodTxStatus;
-        }
-
-        mergedDeposits[txHash].txSource = "openapi";
-    });
-
-    return Object.values(mergedDeposits);
-}
-
-function mergeWithdraws(
-    openapiExits,
-    openapiBurns,
-    watchgodExits,
-    watchgodBurns
-) {
-    const mergedWithdraws = {};
-
-    watchgodBurns.forEach((tx) => {
-        const txHash = tx.txHash;
-
-        mergedWithdraws[txHash] = tx;
-        mergedWithdraws[txHash]._txSource = "watchgod_burns";
-        mergedWithdraws[txHash]._burnTxHash = tx.txHash;
-        mergedWithdraws[txHash]._watchgodBurnTxStatus = tx.txStatus;
-        mergedWithdraws[txHash]._latestStatus = tx.txStatus;
-    });
-
-    openapiBurns.forEach((tx) => {
-        const txHash = tx.txHash;
-        const watchgodBurnTxStatus =
-            mergedWithdraws[txHash] && mergedWithdraws[txHash]._watchgodBurnTxStatus
-                ? mergedWithdraws[txHash]._watchgodBurnTxStatus
-                : false;
-
-        mergedWithdraws[txHash] = tx;
-        mergedWithdraws[txHash]._txSource = "openapi_burns";
-        mergedWithdraws[txHash]._burnTxHash = tx.txHash;
-        mergedWithdraws[txHash]._openapiBurnTxStatus = tx.txStatus;
-        mergedWithdraws[txHash]._latestStatus = tx.txStatus;
-
-        if (watchgodBurnTxStatus) {
-            mergedWithdraws[txHash]._watchgodBurnTxStatus = watchgodBurnTxStatus;
-        }
-    });
-
-    watchgodExits.forEach((tx) => {
-        const txHash = tx.txBurnHash;
-
-        const watchgodBurnTxStatus =
-            mergedWithdraws[txHash] && mergedWithdraws[txHash]._watchgodBurnTxStatus
-                ? mergedWithdraws[txHash]._watchgodBurnTxStatus
-                : false;
-
-        const openapiBurnTxStatus =
-            mergedWithdraws[txHash] && mergedWithdraws[txHash]._openapiBurnTxStatus
-                ? mergedWithdraws[txHash]._openapiBurnTxStatus
-                : false;
-
-        mergedWithdraws[txHash] = tx;
-        mergedWithdraws[txHash]._txSource = "watchgod_exits";
-        mergedWithdraws[txHash]._burnTxHash = tx.txBurnHash;
-        mergedWithdraws[txHash]._exitTxHash = tx.txHash;
-        mergedWithdraws[txHash]._watchgodExitTxStatus = tx.txStatus;
-        mergedWithdraws[txHash]._latestStatus = tx.txStatus;
-
-        if (watchgodBurnTxStatus) {
-            mergedWithdraws[txHash]._watchgodBurnTxStatus = watchgodBurnTxStatus;
-        }
-
-        if (openapiBurnTxStatus) {
-            mergedWithdraws[txHash]._openapiBurnTxStatus = openapiBurnTxStatus;
-        }
-    });
-
-    openapiExits.forEach((tx) => {
-        const txHash = tx.txBurnHash;
-
-        const watchgodBurnTxStatus =
-            mergedWithdraws[txHash] && mergedWithdraws[txHash]._watchgodBurnTxStatus
-                ? mergedWithdraws[txHash]._watchgodBurnTxStatus
-                : false;
-
-        const openapiBurnTxStatus =
-            mergedWithdraws[txHash] && mergedWithdraws[txHash]._openapiBurnTxStatus
-                ? mergedWithdraws[txHash]._openapiBurnTxStatus
-                : false;
-
-        const watchgodExitTxStatus =
-            mergedWithdraws[txHash] && mergedWithdraws[txHash]._watchgodExitTxStatus
-                ? mergedWithdraws[txHash]._watchgodExitTxStatus
-                : false;
-
-        mergedWithdraws[txHash] = tx;
-        mergedWithdraws[txHash]._txSource = "openapi_exits";
-        mergedWithdraws[txHash]._burnTxHash = tx.txBurnHash;
-        mergedWithdraws[txHash]._exitTxHash = tx.txHash;
-        mergedWithdraws[txHash]._openapiExitTxStatus = tx.txStatus;
-        mergedWithdraws[txHash]._latestStatus = tx.txStatus;
-
-        if (watchgodBurnTxStatus) {
-            mergedWithdraws[txHash]._watchgodBurnTxStatus = watchgodBurnTxStatus;
-        }
-
-        if (openapiBurnTxStatus) {
-            mergedWithdraws[txHash]._openapiBurnTxStatus = openapiBurnTxStatus;
-        }
-
-        if (watchgodExitTxStatus) {
-            mergedWithdraws[txHash]._watchgodExitTxStatus = watchgodExitTxStatus;
-        }
-    });
-
-    return Object.values(mergedWithdraws);
 }
 
 module.exports = merger;
---
+++ mergeWithdraws.js
@@ -0,0 +1,109 @@
+function mergeWithdraws(
+    openapiExits,
+    openapiBurns,
+    watchgodExits,
+    watchgodBurns
+) {
+    const mergedWithdraws = {};
+
+    watchgodBurns.forEach((tx) => {
+        const txHash = tx.txHash;
+
+        mergedWithdraws[txHash] = tx;
+        mergedWithdraws[txHash]._txSource = "watchgod_burns";
+        mergedWithdraws[txHash]._burnTxHash = tx.txHash;
+        mergedWithdraws[txHash]._watchgodBurnTxStatus = tx.txStatus;
+        mergedWithdraws[txHash]._latestStatus = tx.txStatus;
+    });
+
+    openapiBurns.forEach((tx) => {
+        const txHash = tx.txHash;
+        const watchgodBurnTxStatus =
+            mergedWithdraws[txHash] && mergedWithdraws[txHash]._watchgodBurnTxStatus
+                ? mergedWithdraws[txHash]._watchgodBurnTxStatus
+                : false;
+
+        mergedWithdraws[txHash] = tx;
+        mergedWithdraws[txHash]._txSource = "openapi_burns";
+        mergedWithdraws[txHash]._burnTxHash = tx.txHash;
+        mergedWithdraws[txHash]._openapiBurnTxStatus = tx.txStatus;
+        mergedWithdraws[txHash]._latestStatus = tx.txStatus;
+
+        if (watchgodBurnTxStatus) {
+            mergedWithdraws[txHash]._watchgodBurnTxStatus = watchgodBurnTxStatus;
+        }
+    });
+
+    watchgodExits.forEach((tx) => {
+        const txHash = tx.txBurnHash;
+
+        const watchgodBurnTxStatus =
+            mergedWithdraws[txHash] && mergedWithdraws[txHash]._watchgodBurnTxStatus
+                ? mergedWithdraws[txHash]._watchgodBurnTxStatus
+                : false;
+
+        const openapiBurnTxStatus =
+            mergedWithdraws[txHash] && mergedWithdraws[txHash]._openapiBurnTxStatus
+                ? mergedWithdraws[txHash]._openapiBurnTxStatus
+                : false;
+
+        mergedWithdraws[txHash] = tx;
+        mergedWithdraws[txHash]._txSource = "watchgod_exits";
+        mergedWithdraws[txHash]._burnTxHash = tx.txBurnHash;
+        mergedWithdraws[txHash]._exitTxHash = tx.txHash;
+        mergedWithdraws[txHash]._watchgodExitTxStatus = tx.txStatus;
+        mergedWithdraws[txHash]._latestStatus = tx.txStatus;
+
+        if (watchgodBurnTxStatus) {
+            mergedWithdraws[txHash]._watchgodBurnTxStatus = watchgodBurnTxStatus;
+        }
+
+        if (openapiBurnTxStatus) {
+            mergedWithdraws[txHash]._openapiBurnTxStatus = openapiBurnTxStatus;
+        }
+    });
+
+    openapiExits.forEach((tx) => {
+        const txHash = tx.txBurnHash;
+
+        const watchgodBurnTxStatus =
+            mergedWithdraws[txHash] && mergedWithdraws[txHash]._watchgodBurnTxStatus
+                ? mergedWithdraws[txHash]._watchgodBurnTxStatus
+                : false;
+
+        const openapiBurnTxStatus =
+            mergedWithdraws[txHash] && mergedWithdraws[txHash]._openapiBurnTxStatus
+                ? mergedWithdraws[txHash]._openapiBurnTxStatus
+                : false;
+
+        const watchgodExitTxStatus =
+            mergedWithdraws[txHash] && mergedWithdraws[txHash]._watchgodExitTxStatus
+                ? mergedWithdraws[txHash]._watchgodExitTxStatus
+                : false;
+
+        mergedWithdraws[txHash] = tx;
+        mergedWithdraws[txHash]._txSource = "openapi_exits";
+        mergedWithdraws[txHash]._burnTxHash = tx.txBurnHash;
+        mergedWithdraws[txHash]._exitTxHash = tx.txHash;
+        mergedWithdraws[txHash]._openapiExitTxStatus = tx.txStatus;
+        mergedWithdraws[txHash]._latestStatus = tx.txStatus;
+
+        if (watchgodBurnTxStatus) {
+            mergedWithdraws[txHash]._watchgodBurnTxStatus = watchgodBurnTxStatus;
+        }
+
+        if (openapiBurnTxStatus) {
+            mergedWithdraws[txHash]._openapiBurnTxStatus = openapiBurnTxStatus;
+        }
+
+        if (watchgodExitTxStatus) {
+            mergedWithdraws[txHash]._watchgodExitTxStatus = watchgodExitTxStatus;
+        }
+    });
+
+    return Object.values(mergedWithdraws);
+}
+
+module.exports = {
+    mergeWithdraws
+}
