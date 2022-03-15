405efab0d228c5de162f1ec2b8d050490c195a97
ankitboghra <ankitboghra@gmail.com>
2022-03-16 01:11:46 +0530

    new: plasma withdraw support for confirm exit txs

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 export const TX_SOURCE = Object.freeze({
     WATCHGOD_DEPOSITS: 'watchgod_deposits',
     WATCHGOD_BURNS: 'watchgod_burns',
+    WATCHGOD_CONFIRM_EXITS: 'watchgod_confirm_exits',
     WATCHGOD_EXITS: 'watchgod_exits',
     OPENAPI_DEPOSITS: 'openapi_deposits',
     OPENAPI_BURNS: 'openapi_burns',
@@ -10,5 +11,6 @@ export const TX_SOURCE = Object.freeze({
 export const TX_TYPE = Object.freeze({
     DEPOSIT: 'deposit',
     WITHDRAW: 'withdraw',
+    ConfirmExit: 'confirm_exit',
     EXIT: 'exit',
 })
