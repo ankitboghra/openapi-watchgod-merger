0d8811a6aae602142a99e83486d4985670d5fbd3
ankitboghra <ankitboghra@gmail.com>
2022-06-07 18:43:36 +0530

    new: watchgod api schema changed, clear extra details

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import mergeOpenApiWatchgod from "./mergeOpenApiWatchgod"
 
-// import {
-//     openApiDepositResponse,
-//     openApiWithdrawResponse,
-//     openApiBurnResponse,
-//     watchGodResponse,
-// } from "../responseData"
+import {
+    openApiDepositResponse,
+    openApiWithdrawResponse,
+    openApiBurnResponse,
+    watchGodResponse,
+} from "../responseData"
 
-// const mergedData = mergeOpenApiWatchgod(
-//     openApiDepositResponse,
-//     openApiWithdrawResponse,
-//     openApiBurnResponse,
-//     watchGodResponse
-// );
-// console.clear()
-// console.time('mergeOpenApiWatchgod')
-// console.log(mergedData);
-// console.timeEnd('mergeOpenApiWatchgod')
+const mergedData = mergeOpenApiWatchgod(
+    openApiDepositResponse,
+    openApiWithdrawResponse,
+    openApiBurnResponse,
+    watchGodResponse
+);
+console.clear()
+console.time('mergeOpenApiWatchgod')
+mergedData.forEach((tx) => console.table(tx));
+console.timeEnd('mergeOpenApiWatchgod')
 
 export default mergeOpenApiWatchgod
 export { TX_SOURCE, TX_TYPE } from "./constants"
